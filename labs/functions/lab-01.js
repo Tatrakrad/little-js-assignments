858690727abb2789f0b25b93ca5408aa2a1c94a4
Tatrakrad <riddick.taylor@gmail.com>
2016-11-07 14:52:59 -0600

    function labs started

---
+++ labs/functions/lab-01.js
@@ -0,0 +1,12 @@
+//function goes here
+function sumLab(numA,numB){
+  return console.log(numA+numB);
+}
+a = 5;
+b = 7;
+
+sumLab(5,7);
+
+/*
+Write a function that takes two parameters then computes and logs the sum
+*/
---
+++ labs/functions/lab-02.js
@@ -0,0 +1,6 @@
+//function goes here
+
+
+/*
+Write a function that takes three parameters and computes and logs the average.
+*/
